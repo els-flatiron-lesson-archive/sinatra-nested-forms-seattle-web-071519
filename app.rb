31c0ed275b370f22dd269cf3bb1173c5dcc64f55
marshallhelenm <marshallhelenm@gmail.com>
2019-08-08 10:24:00 -0700

    Done.

--- app.rb
+++ app.rb
@@ -17,10 +17,6 @@ module FormsLab
       @weight = pirate[:weight]
       @height = pirate[:height]
       @ships = pirate[:ships]
-      # @pirate = Pirate.new(name: params[pirate[name]], weight: params[pirate[weight]], height: params[pirate[height]])
-      # @ship1 = Ship.new(name: params[pirate][:ships][0][:name], type: params[pirate][:ships][0][:type], booty: params[pirate][:ships][0][:booty])
-      # @ship2 = Ship.new(name: params[pirate][:ships][1][:name], type: params[pirate][:ships][1][:type], booty: params[pirate][:ships][1][:booty])
-
       erb :'pirates/show'
     end
 
